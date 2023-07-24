bb86ecf671f9559469a726770ce21bb3be0d7c56
xychen35 <93174367+xychen35@users.noreply.github.com>
2023-07-24 17:59:03 -0400

    Delete music_controler directory

--- music_controler/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Room
-
-# Room Serializer
-class RoomSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Room
-    # fields = '__all__'
-    fields = ('id', 'code', 'host', 'guest_can_pause', 'votes_to_skip', 'created_at')
