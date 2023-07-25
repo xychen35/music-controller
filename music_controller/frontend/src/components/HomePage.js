e4857b530bf4047c8fab6422c98cf40be7879ac0
xychen35 <xiaoyangc99@gmail.com>
2023-07-25 05:12:34 +0000

    frontend updated

---
+++ music_controller/frontend/src/components/HomePage.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+
+export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<p>This is Home Page</p>} />
+          <Route path="/join" element={<RoomJoinPage />} />
+          <Route path="/create" element={<CreateRoomPage />} />
+        </Routes>
+      </Router>
+    );
+  }
+}
