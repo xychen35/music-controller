f3a668ed897ab71344624f4784c4ab9e1909b335
xychen35 <xiaoyangc99@gmail.com>
2023-07-29 04:59:12 +0000

    updated JoinRoom Page

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -12,7 +12,7 @@ class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
 
-class getRoom(APIView):
+class GetRoom(APIView):
     serializer_class = Room
     lookup_url_kwarg = 'code'
 
@@ -27,6 +27,24 @@ class getRoom(APIView):
             return Response({"Room not Found": "Invalid Room Code."}, status=status.HTTP_404_NOT_FOUND)
         
         return Response({"Bad Request": "Code parameter not found in request"}, status=status.HTTP_400_BAD_REQUEST)
+
+class JoinRoom(APIView):
+    lookup_url_kwarg = 'code'
+
+    def post(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+        
+        code = request.data.get(self.lookup_url_kwarg)
+        if code:
+            room_result = Room.objects.filter(code=code)
+            if len(room_result) > 0:
+                room = room_result[0]
+                self.request.session['room_code'] = code
+                return Response({"message": "Room Joined!"}, status=status.HTTP_200_OK)
+            return Response({"Room not Found": "Invalid Room Code."}, status=status.HTTP_404_NOT_FOUND)
+        
+        return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CreateRoomView(APIView):
@@ -47,11 +65,13 @@ class CreateRoomView(APIView):
                 room.guest_can_pause = guest_can_pause
                 room.votes_to_skip = votes_to_skip
                 room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+                self.request.session['room_code'] = room.code
                 return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
             else:
                 room = Room(host=host, guest_can_pause=guest_can_pause,
                             votes_to_skip=votes_to_skip)
                 room.save()
+                self.request.session['room_code'] = room.code
                 return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
 
         return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
--- music_controller/frontend/src/components/RoomJoinPage.js
+++ music_controller/frontend/src/components/RoomJoinPage.js
@@ -1,10 +1,66 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Grid, Typography, TextField, Button } from "@mui/material";
 
-export default class RoomJoinPage extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return <p>RoomJoinPage</p>;
-  }
+export default function RoomJoinPage(props) {
+  const navigate = useNavigate();
+
+  const [roomCode, setRoomCode] = useState("");
+  const [error, setError] = useState("");
+
+  const handleTextFieldChange = (e) => {
+    setRoomCode(e.target.value);
+  };
+
+  const roomButtonPressed = (e) => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        code: roomCode,
+      }),
+    };
+    fetch("/api/join-room", requestOptions)
+      .then((response) => {
+        if (response.ok) {
+          navigate(`/room/${roomCode}`);
+        } else {
+          setError("Room not found.");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <Grid container spacing={1} align="center">
+      <Grid item xs={12}>
+        <Typography variant="h4" component="h4">
+          Join a Room
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          error={error}
+          label="Code"
+          placeholder="Enter a Room Code"
+          value={roomCode}
+          helperText={error}
+          variant="outlined"
+          onChange={handleTextFieldChange}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Button variant="contained" color="primary" onClick={roomButtonPressed}>
+          Enter Room
+        </Button>
+      </Grid>
+      <Grid item xs={12}>
+        <Button variant="contained" color="secondary" to="/" component={Link}>
+          Back
+        </Button>
+      </Grid>
+    </Grid>
+  );
 }
