adf6c34e49aaca9b25ddf7d89be02f4660e1011c
xychen35 <xiaoyangc99@gmail.com>
2023-07-30 03:01:56 +0000

    updated HomePage

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -7,19 +7,60 @@ import {
   Routes,
   Route,
   Link,
-  Redirect,
+  Navigate,
 } from "react-router-dom";
+import { Grid, Button, ButtonGroup, Typography } from "@mui/material";
 
 export default class HomePage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      roomCode: null,
+    };
   }
 
+  async componentDidMount() {
+    fetch("/api/user-in-room")
+      .then((response) => response.json())
+      .then((data) =>
+        this.setState({
+          roomCode: data.code,
+        })
+      );
+  }
+
+  renderHomePage() {
+    if (this.state.roomCode) {
+      return (
+        <Navigate to={`/room/${this.state.roomCode}`} replace={true}></Navigate>
+      );
+    } else {
+      return (
+        <Grid container spacing={3} align="center">
+          <Grid item xs={12}>
+            <Typography variant="h3" compact="h3">
+              Music House
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <ButtonGroup disableElevation variant="contained" color="primary">
+              <Button color="primary" to="/join" component={Link}>
+                Join a Room
+              </Button>
+              <Button color="secondary" to="/create" component={Link}>
+                Create a Room
+              </Button>
+            </ButtonGroup>
+          </Grid>
+        </Grid>
+      );
+    }
+  }
   render() {
     return (
       <Router>
         <Routes>
-          <Route exact path="/" element={<p>This is Home Page</p>} />
+          <Route exact path="/" element={this.renderHomePage()} />
           <Route path="/join" element={<RoomJoinPage />} />
           <Route path="/create" element={<CreateRoomPage />} />
           <Route path="/room/:roomCode" element={<Room />} />
