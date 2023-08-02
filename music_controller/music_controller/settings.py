3032efb435481001e8e6dcd685533b4dd97f7b75
xychen35 <xiaoyangc99@gmail.com>
2023-08-02 14:38:20 +0000

    spotify init

--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api.apps.ApiConfig',
+    'api',
     'rest_framework',
-    'frontend.apps.FrontendConfig',
+    'frontend',
+    'spotify',
 ]
 
 MIDDLEWARE = [
---
+++ music_controller/spotify/credentials.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+REDIRECT_URI = os.getenv("REDIRECT_URI")
